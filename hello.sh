4020fa8782abe11f88ee8b94e9f4a184ebb2369c
Sergey Trubnikov <sergeytrubnikov1@gmail.com>
2021-05-01 21:50:49 +0300

    Get back only one hello world to hello.sh

--- hello.sh
+++ hello.sh
@@ -2,18 +2,3 @@
 
 echo "hello world!"
 
-echo "Hello again! Friends!" 
-
-echo "Here we go again"
-echo "Here we go again"
-echo "Here we go again"
-echo "Here we go again"
-echo "Here we go again"
-echo "Here we go again"
-echo "Here we go again"
-echo "Here we go again2"
-echo "Here we go again3"
-echo "Here we go again3"
-echo "Here we go again3"
-echo "Here we go again3"
-echo "Here we go again123456"
